2a31b1de8950e109dab893793674d209a0ad66a0
rahabulislam1 <rahabulislam1@gmail.com>
2022-02-02 17:37:08 +0600

    Odd-Even Solved

---
+++ odd-even.js
@@ -0,0 +1,13 @@
+function oddEvenCheck(num) {
+    if (num % 2 == 0) {
+        // result = 'Even';
+        return 'Even';
+    }
+    else if (num % 2 == 1) {
+        // result = 'Odd';
+        return 'Odd';
+    }
+}
+var num = 34;
+var checkNumber = oddEvenCheck(num);
+console.log(checkNumber);
